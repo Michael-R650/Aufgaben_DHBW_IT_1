17d020aabedb5b30166efc64c76b1d9fe758e07e
Michael-R650 <reichle.michae-it25@it.dhbw-ravensburg.de>
2026-02-15 16:32:05 +0100

    Aufgabe L5 erledigt

--- Aufgaben/Aufgabe_L5/Section3.c
+++ Aufgaben/Aufgabe_L5/Section3.c
@@ -4,62 +4,75 @@
 
 int main(int argc, char **acrgv)
 {
-    const int LENGTH = 9;
+    const int MAX = 9;
+    const int MIN = 0;
     srand((unsigned int)time(NULL));
     int gametable[10] = {0};
     int next_simulation[10] = {0};
     int *p_gametable = gametable;
     int *p_next_Simulation = next_simulation;
 
+    int anzahl = 3;
+    int steps = 10;
+    int time = 0;
     *(p_gametable + 2) = 1;
     *(p_gametable + 4) = 1;
     *(p_gametable + 6) = 1;
-    int steps = 10;
-    int time = 0;
 
-    for (int i = 0; i < 10; i++)
-    {
-
-        for (int i = 0; i < (steps); i++)
-        {
-            if (*(p_gametable + i) == 1)
-            {
-
-                if (rand() % 2)
-                {printf("%d->%d\n", i, i - 1);
-
-                    if (i != 0)
-                    {
-                        
-                        *(p_gametable + i) = 0;
-                        *(p_next_Simulation - 1 + i) = 1;
-                    }
-                }
-                else
-                {printf("%d->%d\n", i, i + 1);
-                    if (i != LENGTH)
-                    {
-                        
-                        *(p_gametable + 1 + i) = 0;
-                        *(p_next_Simulation + 1 + i) = 1;
-                    }
-                    // printf("%d\n",i);
-                }
-                //*(p_gametable+i)=0;
-            }
-        }
-
-        for (int i = 0; i < steps; i++)
-        {
-            *(p_gametable + i) = *(p_next_Simulation + i);
-            *(p_next_Simulation+i)=0;
-        }
-        time++;
-        printf("Time :%d ", time);
+    // Schleife, solange es mehr als eine 1 auf dem Spielfeld gibt
+    while (anzahl > 1)
+    {//Ausge der Zeit,Anzahl der Einsen und des Spielbrettes
+        printf("Time :%d ,Anzahl: %d Spielbrett-> ", time, anzahl);
         for (int i = 0; i < steps; i++)
         {
             printf("%d ", *(p_gametable + i));
         }
         printf("\n");
+        anzahl = 0;
+        // Schleife welche alle Elemente des Arrays durchläuft
+        for (int i = 0; i < (steps); i++)
+        { // Abfrage, ob ein Element eine 1 ist
+            if (*(p_gametable + i) == 1)
+            {
+                anzahl++;
+                // Entscheidung, ob ein Element nach rechts oder links geht
+                if (rand() % 2)
+                {
+
+                    if (i > MIN)
+                    {
+                        *((p_next_Simulation + i - 1)) += 1;
+                    }
+                    else
+                    {
+                        *((p_next_Simulation + i)) += 1;
+                    }
+                }
+                else
+                {
+
+                    if (i < MAX)
+                    {
+                        *((p_next_Simulation + 1 + i)) += 1;
+                    }
+                    else
+                    {
+                        *((p_next_Simulation + i)) += 1;
+                    }
+                }
+            }
+        }
+        // Schleife zum übertragen des temporären Arrays zum anderen
+        for (int i = 0; i < steps; i++)
+        {
+            if (*(p_next_Simulation + i) > 1)
+            {
+                printf("Collision on Index %d\n", i);
+                *(p_next_Simulation + i) = 0;
+            }
+            *(p_gametable + i) = *(p_next_Simulation + i);
+            *(p_next_Simulation + i) = 0;
+        }
+        time++;
     }
 }
